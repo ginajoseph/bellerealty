9d9072b62c5bd255cf9b1fbbd405c07b62f9e1ea
Gina Joseph <ginajoseph01@aol.com>
2014-10-19 10:18:32 -0400

    changed text to register

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
   root 'visitors#index'
   devise_for :users
   resources :users
+
+  devise_scope :users do
+    match '/sessions' => 'sessions#create', :via => :post
+    match '/sessions' => 'sessions#destroy', :via => :delete
+  end
 end
