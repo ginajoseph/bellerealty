fbc4d5c2c481a4f65c6837f854f7c2277bee4929
Gina Joseph <ginajoseph01@aol.com>
2014-10-23 22:55:16 -0400

    removed devise session route

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,4 @@ Rails.application.routes.draw do
   root 'visitors#index'
   devise_for :users
   resources :users
-
-  devise_scope :users do
-    match '/sessions' => 'sessions#create', :via => :post
-    match '/sessions' => 'sessions#destroy', :via => :delete
-  end
 end
